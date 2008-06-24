b54fc2f9715a1a63d0f758446b1b03c19bc60246
John Luxford <lux@www.sitellite-4.2.13-stable.lo>
2008-06-24 00:06:48 -0500

    Added the ability to enable/disable non-core apps in the admin menu.

---
+++ inc/conf/auth/applications/index.php
@@ -0,0 +1,58 @@
+; <?php /* DO NOT ALTER THIS LINE, IT IS HERE FOR SECURITY REASONS
+;
+; WARNING: This file was automatically generated, and it may
+; not be wise to edit it by hand.  If there is an interface
+; to modify files of this type, please use that interface
+; instead of manually editing this file.  If you are not sure
+; or are not aware of such an interface, please talk to your
+; Sitellite administrator first.
+;
+
+boxchooser              = core
+
+cms                     = core
+
+filechooser             = core
+
+formchooser             = core
+
+imagechooser            = core
+
+pagebrowser             = core
+
+scheduler               = core
+
+sitemember              = core
+
+help                    = core
+
+multilingual            = core
+
+upgrade                 = core
+
+usradm                  = core
+
+sitellite               = core
+
+wffolderbrowser         = core
+
+xed                     = core
+
+appdoc                  = On
+
+myadm                   = On
+
+devfiles                = On
+
+devnotes                = On
+
+examples                = On
+
+news                    = On
+
+webapp                  = On
+
+;
+; THE END
+;
+; DO NOT ALTER THIS LINE, IT IS HERE FOR SECURITY REASONS */ ?>
