b64063ae3aed71ca60490c2332482ec422fac3cd
John Luxford <lux@www.semias-gmap.lo>
2008-10-06 09:48:40 -0500

    Fixed syntax error

--- inc/app/upgrade/boxes/5_0_2/index.php
+++ inc/app/upgrade/boxes/5_0_2/index.php
@@ -2,7 +2,7 @@
 
 if (upgrade_ran ('5.0.2')) {
 	page_title (intl_get ('Upgrade Already Applied') . ' (5.0.2)');
-	echo '<p><a href="' . site_prefix () . '/index/upgrade-app">') . intl_get ('Back') . '</a></p>';
+	echo '<p><a href="' . site_prefix () . '/index/upgrade-app">' . intl_get ('Back') . '</a></p>';
 	return;
 }
 
