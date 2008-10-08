dde93066ccddee97f52b56fc4859371b67a700f2
Charles Brunet <cbrunet@fmjs.(none)>
2008-10-08 09:49:21 -0400

    I18n of dates (first draft)

---
+++ inc/lang/fr.dates.php
@@ -0,0 +1,27 @@
+; <?php /* DO NOT ALTER THIS LINE, IT IS HERE FOR SECURITY REASONS
+;
+
+[translations]
+days = dimanche, lundi, mardi, mercredi, jeudi, vendredi, samedi
+shortdays = dim, lun, mar, mer, jeu, ven, sam
+suffixes = er,,,
+months = janvier, février, mars, avril, mai, juin, juillet, août, septembre, novembre, décembre
+shortmonths = jan, fév, mar, avr, mai, jun, jul, aoû, sep, oct, nov, déc
+antepost = am, pm
+
+[formats]
+shorttime = H\hi
+time = H:i:s
+shortdate = y/m/d
+date = n M. Y
+longdate = n F Y
+daydate = D n M. Y
+longdaydate = l n F Y
+datetime = y/m/d H:i:s
+longdatetime = l n F Y H:i:s
+
+;
+; THE END
+;
+; DO NOT ALTER THIS LINE, IT IS HERE FOR SECURITY REASONS */ ?>
+
