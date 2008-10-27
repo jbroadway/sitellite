5b27c66205589445caada9d3c2dac2b032a054bf
Charles Brunet <cbrunet@fmjs.(none)>
2008-10-27 09:56:18 -0400

    minor changes

--- inc/lang/en.dates.php
+++ inc/lang/en.dates.php
@@ -1,27 +1,48 @@
 ; <?php /* DO NOT ALTER THIS LINE, IT IS HERE FOR SECURITY REASONS
 ;
+; WARNING: This file was automatically generated, and it may
+; not be wise to edit it by hand.  If there is an interface
+; to modify files of this type, please use that interface
+; instead of manually editing this file.  If you are not sure
+; or are not aware of such an interface, please talk to your
+; Sitellite administrator first.
+;
 
 [translations]
-days = Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
-shortdays = Sun, Mon, Tue, Wed, Thu, Fri, Sat
-suffixes = st, nd, rd, th
-months = January, February, March, April, May, June, July, August, September, October, November, December
-shortmonths = Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec
-antepost = am, pm
+
+days                    = "Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday"
+
+shortdays               = "Sun, Mon, Tue, Wed, Thu, Fri, Sat"
+
+suffixes                = "st, nd, rd, th"
+
+months                  = "January, February, March, April, May, June, July, August, September, October, November, December"
+
+shortmonths             = "Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec"
+
+antepost                = "am, pm"
 
 [formats]
-shorttime = h:i a
-time = h:i:s a
-shortdate = m-d-y
-date = M d, Y
-longdate = F d, Y
-daydate = D, M d, Y
-longdaydate = l, F d, Y
-datetime = m-d-y h:i:s a
-longdatetime = l, F jS, Y h:i:s a
+
+shorttime               = "h:i a"
+
+time                    = "h:i:s a"
+
+shortdate               = m-d-y
+
+date                    = "M d, Y"
+
+longdate                = "F d, Y"
+
+daydate                 = "D, M d, Y"
+
+longdaydate             = "l, F d, Y"
+
+datetime                = "m-d-y h:i:s a"
+
+longdatetime            = "l, F jS, Y h:i:s a"
 
 ;
 ; THE END
 ;
 ; DO NOT ALTER THIS LINE, IT IS HERE FOR SECURITY REASONS */ ?>
-
--- inc/lang/fr.dates.php
+++ inc/lang/fr.dates.php
@@ -1,27 +1,48 @@
 ; <?php /* DO NOT ALTER THIS LINE, IT IS HERE FOR SECURITY REASONS
 ;
+; WARNING: This file was automatically generated, and it may
+; not be wise to edit it by hand.  If there is an interface
+; to modify files of this type, please use that interface
+; instead of manually editing this file.  If you are not sure
+; or are not aware of such an interface, please talk to your
+; Sitellite administrator first.
+;
 
 [translations]
-days = dimanche, lundi, mardi, mercredi, jeudi, vendredi, samedi
-shortdays = dim, lun, mar, mer, jeu, ven, sam
-suffixes = er,,,
-months = janvier, février, mars, avril, mai, juin, juillet, août, septembre, octobre, novembre, décembre
-shortmonths = jan, fév, mar, avr, mai, jun, jul, aoû, sep, oct, nov, déc
-antepost = am, pm
+
+days                    = "dimanche, lundi, mardi, mercredi, jeudi, vendredi, samedi"
+
+shortdays               = "dim, lun, mar, mer, jeu, ven, sam"
+
+suffixes                = "er,,,"
+
+months                  = "janvier, février, mars, avril, mai, juin, juillet, août, septembre, octobre, novembre, décembre"
+
+shortmonths             = "jan, fév, mar, avr, mai, jun, jul, aoû, sep, oct, nov, déc"
+
+antepost                = "am, pm"
 
 [formats]
-shorttime = H\hi
-time = H:i:s
-shortdate = y/m/d
-date = n M. Y
-longdate = n F Y
-daydate = D n M. Y
-longdaydate = l n F Y
-datetime = y/m/d H:i:s
-longdatetime = l jS F Y H:i:s
+
+shorttime               = "H\hi"
+
+time                    = "H:i:s"
+
+shortdate               = y/m/d
+
+date                    = n M. Y
+
+longdate                = n F Y
+
+daydate                 = D n M. Y
+
+longdaydate             = l n F Y
+
+datetime                = "y/m/d H:i:s"
+
+longdatetime            = "l jS F Y H:i:s"
 
 ;
 ; THE END
 ;
 ; DO NOT ALTER THIS LINE, IT IS HERE FOR SECURITY REASONS */ ?>
-
