f9a90ec966fc0efbf6cec9da84c932e583c9a634
John Luxford <lux@www.semias-gmap.lo>
2008-11-11 13:08:12 -0600

    SiteStreamer: Added optional integration with the JW Flash Player.

--- inc/app/sitestreamer/lib/flash.php
+++ inc/app/sitestreamer/lib/flash.php
@@ -13,6 +13,33 @@ function sitestreamer_flvheader () {
 }
 
 function sitestreamer_flash ($file, $width, $height, $fg = '0x444444', $bg = '0xAAAAAA') {
+	static $num = 0;
+	$num++;
+	if (@file_exists ('inc/app/sitestreamer/lib/swfobject.js')) {
+		$image = str_replace ('.flv', '.jpg', $file);
+		if (! @file_exists ('inc/data' . $image)) {
+			$image = '';
+		}
+		page_add_script (site_prefix () . '/inc/app/sitestreamer/lib/swfobject.js');
+		return template_simple (
+			'<p align="center" class="sitestreamer-video" id="sitestreamer-video{num}"><a href="http://www.macromedia.com/go/getflashplayer">You need the Flash Player to watch this video.</a></p>
+			<script type="text/javascript">
+				var swfobj{num} = new SWFObject ("{site/prefix}/inc/app/sitestreamer/lib/player.swf", "sitestreamer-video", "{width}", "{height}", "9", "{bg}");
+				swfobj{num}.addParam ("allowfullscreen","true");
+				swfobj{num}.addParam ("flashvars", "file={site/prefix}/index/cms-filesystem-action{file}{if not empty (obj[image])}&image={site/prefix}/index/cms-filesystem-action{image}{end if}");
+				swfobj{num}.write ("sitestreamer-video{num}");
+			</script>',
+			array (
+				'file' => $file,
+				'image' => $image,
+				'width' => $width,
+				'height' => $height,
+				'fg' => $fg,
+				'bg' => $bg,
+				'num' => $num,
+			)
+		);
+	}
 	sitestreamer_flvheader ();
 
 	$height += 40;
