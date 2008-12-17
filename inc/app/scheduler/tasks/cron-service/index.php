d4ef3df8f0d63e50f074b7e9bd26eb79f43c0733
Charles Brunet <cbrunet@fmjs.(none)>
2008-12-18 00:49:36 +0800

    Added cron scheduler task to trigger workflow service at specific interval
    Signed-off-by: Charles Brunet <charles.fmj@gmail.com>

---
+++ inc/app/scheduler/tasks/cron-service/index.php
@@ -0,0 +1,28 @@
+<?php
+
+// BEGIN CLI KEEPOUT CHECKING
+if (php_sapi_name () !== 'cli') {
+    // Add these lines to the very top of any file you don't want people to
+    // be able to access directly.
+    header ('HTTP/1.1 404 Not Found');
+    echo "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">\n"
+        . "<html><head>\n<title>404 Not Found</title>\n</head><body>\n<h1>Not Found</h1>\n"
+        . "The requested URL " . $PHP_SELF . " was not found on this server.<p>\n<hr>\n"
+        . $_SERVER['SERVER_SIGNATURE'] . "</body></html>";
+    exit;
+}
+// END CLI KEEPOUT CHECKING
+
+loader_import ('cms.Workflow');
+
+$date = date('Y-m-d H:m:s');
+
+echo Workflow::trigger (
+	'cron',
+	array (
+		'data'    => $date,
+		'message' => 'cron event: ' . $date,
+	)
+);
+
+?>
