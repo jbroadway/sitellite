8b4e1434979d2b549546ad0a77207b0e247a4c8f
Charles Brunet <charles@cbrunet.net>
2009-07-24 23:01:08 +0800

    changelog, and minor translation issue
    Signed-off-by: Charles Brunet <charles.fmj@gmail.com>

--- inc/app/sitemember/forms/preferences/settings.php
+++ inc/app/sitemember/forms/preferences/settings.php
@@ -2,70 +2,70 @@
 
 [Form]
 
-message			= "*Required fields"
-error_mode		= all
+message         = "*Required fields"
+error_mode      = all
 
 [email]
 
-type			= text
-alt				= "*Email"
-rule 1			= "not empty, You must enter an email address."
+type            = text
+alt             = "*Email"
+rule 1          = "not empty, You must enter an email address."
 
 [firstname]
 
-type			= text
-alt				= "*First Name"
+type            = text
+alt             = "*First Name"
 rule 1          = "not empty, You must enter your first name."
 
 [lastname]
 
-type			= text
-alt				= "*Last Name"
+type            = text
+alt             = "*Last Name"
 rule 1          = "not empty, You must enter your last name."
 
 [company]
 
-type			= text
-alt				= Company
+type            = text
+alt             = Company
 
 [website]
 
-type			= text
-alt				= Website
-default_value	= "http://"
+type            = text
+alt             = Website
+default_value   = "http://"
 
 [country]
 
-type			= text
-alt				= Pays
+type            = text
+alt             = Country
 
 [province]
 
-type			= text
-alt				= State/Province
+type            = text
+alt             = State/Province
 
 [profile]
 
-type			= textarea
-alt				= "A little about yourself..."
-rows			= 5
+type            = textarea
+alt             = "A little about yourself..."
+rows            = 5
 
 [sig]
 
-type			= textarea
-alt				= "Signature (for comments)"
-rows			= 3
+type            = textarea
+alt             = "Signature (for comments)"
+rows            = 3
 
 [public]
 
-type			= checkbox
-fieldset		= no
-value 1			= Add me to public member list
+type            = checkbox
+fieldset        = no
+value 1         = Add me to public member list
 
 [submit_button]
 
-type		= submit
-setValues	= Save Preferences
-extra		= "class='submit'"
+type            = submit
+setValues       = Save Preferences
+extra           = "class='submit'"
 
 ; */ ?>
