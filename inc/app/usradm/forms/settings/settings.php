ed313ff67d649aed3032a34da326ab379d95fdc2
John Luxford <john.luxford@gmail.com>
2009-07-27 16:51:46 -0500

    Removed 'session' option from language negotiation list [#140].

--- inc/app/usradm/forms/settings/settings.php
+++ inc/app/usradm/forms/settings/settings.php
@@ -193,7 +193,7 @@ formhelp = "Directory where language files are stored."
 
 type = select
 alt = Determine language from
-setValues = "eval: array ('http' => 'Browser settings', 'cookie' => 'Session cookie', 'url' => 'Page URLs (e.g. /fr/)', 'session' => 'Sitellite preferences')"
+setValues = "eval: array ('http' => 'Browser settings', 'cookie' => 'Session cookie', 'url' => 'Page URLs (e.g. /fr/)')"
 formhelp = "The method of determining which language to use for each visitor.  See saf.I18n::negotiate() in the API references for more information."
 
 [I18n_serialize]
