e88635f0d46f7131df024a86f0c87a351a3adfe1
Charles Brunet <charles@cbrunet.net>
2009-08-07 01:56:07 +0800

    siteforum now show statistics according to user permissions
    Signed-off-by: Charles Brunet <charles.fmj@gmail.com>

--- inc/app/siteforum/boxes/index/index.php
+++ inc/app/siteforum/boxes/index/index.php
@@ -28,9 +28,9 @@ echo template_simple (
 		'list' => $list,
 		'users' => db_shift ('select count(*) from sitellite_user'),
 		'active' => db_shift ('select count(*) from sitellite_user where session_id is not null and expires >= ?', date ('Y-m-d H:i:s', time() - 3600)),
-		'posts' => db_shift ('select count(*) from siteforum_post'),
-		'today' => db_shift ('select count(*) from siteforum_post where ts >= ?', date('Y-m-d 00:00:00')),
-		'week' => db_shift ('select count(*) from siteforum_post where ts >= ?', date ('Y-m-d 00:00:00', time () - 604800)),
+		'posts' => db_shift ('select count(*) from siteforum_post WHERE ' . session_allowed_sql ()),
+		'today' => db_shift ('select count(*) from siteforum_post where ts >= ? AND' . session_allowed_sql (), date('Y-m-d 00:00:00')),
+		'week' => db_shift ('select count(*) from siteforum_post where ts >= ? AND ' . session_allowed_sql (), date ('Y-m-d 00:00:00', time () - 604800)),
 		'guests' => $guests,
 	)
 );
