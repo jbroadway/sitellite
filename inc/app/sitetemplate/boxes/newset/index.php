d2e2852552bb3b46ad6d8bd5de2214034075b0da
John Luxford <lux@www.dojolearning.lo>
2009-10-13 15:01:33 -0500

    Added cgi_rewrite_filter to new template sets in SiteTemplate.

--- inc/app/sitetemplate/boxes/newset/index.php
+++ inc/app/sitetemplate/boxes/newset/index.php
@@ -314,8 +314,9 @@ END;
 [html]
 
 content_type	= text/html
-filter 1		= "body: saf.Misc.Search"
-;filter 2		= "body: siteglossary.Terms"
+filter 1		= "final: cgi_rewrite_filter"
+filter 2		= "body: saf.Misc.Search"
+;filter 3		= "body: siteglossary.Terms"
 
 ; */ ?>
 END;
