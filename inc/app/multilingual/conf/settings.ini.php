3fdf3a85669078278ae0c7e93d9508ba9f26eee0
Semias <info@semias.nl>
2012-07-14 01:18:13 +0200

    Updated the Google multilingual translation suggestions
    The Google API no longer allows free translations. The multilingual app
    has been altered to work with a paid version of the Google API. An API
    key can be added in the Admin panl.

---
+++ inc/app/multilingual/conf/settings.ini.php
@@ -0,0 +1,30 @@
+; <?php /* DO NOT ALTER THIS LINE, IT IS HERE FOR SECURITY REASONS
+;
+; WARNING: This file was automatically generated, and it may
+; not be wise to edit it by hand.  If there is an interface
+; to modify files of this type, please use that interface
+; instead of manually editing this file.  If you are not sure
+; or are not aware of such an interface, please talk to your
+; Sitellite administrator first.
+;
+
+[enable_translate_key]
+
+alt                     = Enable Google Translation Api Key
+
+type                    = select
+
+setValues               = "eval: array ('1' => 'Yes', '' => 'No')"
+
+value                   = On
+
+[translate_key]
+
+alt                     = Google Translation Api Key
+
+value                   = 
+
+;
+; THE END
+;
+; DO NOT ALTER THIS LINE, IT IS HERE FOR SECURITY REASONS */ ?>
